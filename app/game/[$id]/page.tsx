50a9314c0e9807a0674b189d22e54592a081d321
Alex Patterson <alex@codingcat.dev>
2022-11-15 14:23:27 -0500

    add subscription

--- app/game/[$id]/page.tsx
+++ app/game/[$id]/page.tsx
@@ -1,20 +1,65 @@
 "use client";
 
-import { Game, getGame } from "@/utils/appwrite.databases.server";
+import { useKeyPress } from "@/hooks/useKeyPress";
+import {
+  createMove,
+  Game,
+  getGame,
+  subscribeGame,
+} from "@/utils/appwrite.databases.server";
 import { useEffect, useState } from "react";
 
 export default function Page({ params }: { params: { $id: string } }) {
   const [game, setGame] = useState<null | Game>(null);
+  const good: boolean = useKeyPress("ArrowLeft");
+  const evil: boolean = useKeyPress("ArrowRight");
 
   useEffect(() => {
+    let unsubscribe: any;
     (async () => {
       try {
-        setGame(await getGame(params.$id));
+        console.log(params?.$id);
+        if (params?.$id) {
+          // Get initial game detail
+          setGame(await getGame(params.$id));
+
+          //setup subscription
+          unsubscribe = subscribeGame(params.$id, (game) => {
+            setGame(game);
+          });
+        }
       } catch (error) {
         console.error(error);
       }
     })();
+    return () => {
+      if (unsubscribe) unsubscribe();
+    };
   }, [params]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        if (game?.$id && good) {
+          createMove(true, game?.$id);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, [game?.$id, good]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        if (game?.$id && evil) {
+          createMove(false, game?.$id);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, [game?.$id, evil]);
 
   return (
     <div className="flex justify-center p-8">
@@ -22,6 +67,16 @@ export default function Page({ params }: { params: { $id: string } }) {
         <h1 className="text-2xl font-bold">
           Game: {game ? game?.name : `Loading...`}
         </h1>
+        <div className="grid grid-cols-2">
+          <div className="grid grid-rows-2">
+            <p className="text-2xl font-bold">Good: {game?.good}</p>
+            <p className="text-8xl">{good && "🦸"}</p>
+          </div>
+          <div className="grid grid-rows-2">
+            <div className="text-2xl font-bold">Evil: {game?.evil}</div>
+            <p className="text-8xl">{evil && "😈"}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- utils/appwrite.databases.server.ts
+++ utils/appwrite.databases.server.ts
@@ -1,4 +1,4 @@
-import { Client, Databases, ID } from "appwrite";
+import { Client, Databases, ID, RealtimeResponseEvent } from "appwrite";
 import { config } from "@/config/appwrite";
 import type { Models } from "appwrite";
 
@@ -10,6 +10,11 @@ client
   .setEndpoint(config.endpoint) // Your API Endpoint
   .setProject(config.projectId); // Your project ID
 export const databases = db;
+export interface Game extends Models.Document {
+  name: string;
+  good: number;
+  evil: number;
+}
 
 export const createGame = (name: string) => {
   return databases.createDocument(
@@ -20,14 +25,35 @@ export const createGame = (name: string) => {
   );
 };
 
-export interface Game extends Models.Document {
-  name: string;
-}
-
 export const getGame = ($id: string) => {
   return databases.getDocument(
     "appwrite-realtime-db",
     "games",
     $id
   ) as Promise<Game>;
+};
+
+export const subscribeGame = (
+  $id: string,
+  cb: (value: Game) => void | PromiseLike<void>
+) => {
+  return client.subscribe(
+    `databases.appwrite-realtime-db.collections.games.documents.${$id}`,
+    (response) => {
+      cb(response.payload as Game);
+    }
+  );
+};
+
+export interface Move extends Models.Document {
+  good: boolean;
+}
+
+export const createMove = (good: boolean, game_id: string) => {
+  return databases.createDocument(
+    "appwrite-realtime-db",
+    "moves",
+    ID.unique(),
+    { good, game_id }
+  );
 };
